88a81c27c1f48c2d32445147f8eefba36b7fb275
addo <forison6205@gmail.com>
2019-11-11 09:34:42 -0500

    linter last two linter error

--- src/component/homepage.js
+++ src/component/homepage.js
@@ -48,7 +48,11 @@ const main = () => {
 	nextAndPrev.appendChild(prevPhoto);
 
 	nextPhoto.addEventListener('click', () => {
-		(i === images.length - 1) ? i = 0 : i += 1;
+		if (i === images.length - 1) {
+			i = 0;
+		} else {
+			i += 1;
+		}
 		containerImgWrap.src = images[i];
 	});
 	prevPhoto.addEventListener('click', () => {
